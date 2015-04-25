0f5459ecddace100b8be50465cce84d223b40d39
Tracy Semmelink <toranome009@gmail.com>
2015-04-25 22:35:00 +0200

    Refactoring
    SequenceGenerator
    - Parameterized input name
    JsonParser
    - Added unit tests
    - Wrapped parsing method in a class

--- JsonParser.py
+++ JsonParser.py
@@ -1,38 +1,77 @@
 import json
+import unittest
 
 filename = 'SampleSequenceInput'
 inputFile = open('{}.json'.format(filename), 'r')
 root = json.load(inputFile)
 inputFile.close();
+print (root)
 
-nodes = []
-edges = []
+class JsonParser:
 
-def PrintStructure(content, parent="", tab=""):
-    if type(content) is dict:
-        if len(content.keys()) == 0:
-            print ("Empty Dict")
+    nodes = []
+    edges = []
+
+    def ParseStructure(content, parent="", tab=""):
+        if type(content) is dict:
+            if len(content.keys()) == 0:
+                print ("Empty Dict")
+            else:
+                results = []
+                for item in content.items():
+                    print ("{}{} -> {}".format(tab, parent, item[0]))
+                    results.append(JsonParser.ParseStructure(
+                        item[1],
+                        item[0],
+                        tab+"  ")
+                    )
+                return results
+        elif type(content) is list:
+            if len(content) == 0:
+                print ("Empty List")
+            else:
+                results = []
+                for item in content:
+                    results.append(JsonParser.ParseStructure(
+                        item,
+                        parent,
+                        tab)
+                    )
+                return results
         else:
-            for item in content.items():
-                print ("{}{} -> {}".format(tab, parent, item[0]))
-                PrintStructure(item[1], item[0], tab+"  ")
-    elif type(content) is list:
-        if len(content) == 0:
-            print ("Empty List")
-        else:
-            for item in content:
-                PrintStructure(item, parent, tab)
-    else:
-        print ("{}{} -> {} : {}".format(tab, parent, content, type(content)))
+            print ("{}{} -> {} : {}".format(
+                tab,
+                parent,
+                content,
+                type(content))
+            )
 
-PrintStructure(root)
+    def PrintNodes():
+        pass
 
-outputFile = open('output/SampleSequenceInput.dot', 'w')
-outputFile.write('digraph Name {')
-outputFile.write('}')
-outputFile.close()
+    def PrintEdges():
+        pass
 
-renderFile = open('output/render_SampleSequenceInput.bat', 'w')
-renderFile.write('dot -Tsvg SampleSequenceInput.dot -o SampleSequenceInput.svg')
-renderFile.close()
+#outputFile = open('output/SampleSequenceInput.dot', 'w')
+#outputFile.write('digraph Name {')
+#outputFile.write('}')
+#outputFile.close()
 
+#renderFile = open('output/render_SampleSequenceInput.bat', 'w')
+#renderFile.write('dot -Tsvg SampleSequenceInput.dot -o SampleSequenceInput.svg')
+#renderFile.close()
+
+class TestJsonParser(unittest.TestCase):
+
+    def setUp(self):
+        self.structure = {'Root':
+                        {'List':
+                            [1,2,3,{'Nested List':
+                                [1,2,3,[4,5]]}]}}
+
+    def test_ParseStructure_AllNodesAreParsed(self):
+        result = JsonParser.ParseStructure(self.structure)
+        self.assertEqual(list, type(result))
+
+if __name__ == '__main__':
+    unittest.main()
--- SequenceGenerator.py
+++ SequenceGenerator.py
@@ -1,7 +1,12 @@
 import json
 
-filename = 'SampleSequenceInput'
-inputFile = open('SampleSequenceInput.json', 'r')
+willWriteFiles = True
+willWriteBatchFiles = False
+outputFolder = 'output'
+fileName = 'SampleSequenceInput'
+
+inputFileName = '{}.json'.format(fileName)
+inputFile = open(inputFileName, 'r')
 root = json.load(inputFile)
 inputFile.close();
 
@@ -23,12 +28,18 @@ for c in connections:
 for f in frames:
     print (f)
 
-outputFile = open('output/SampleSequenceInput.dot', 'w')
-outputFile.write('digraph Name {')
-outputFile.write('}')
-outputFile.close()
+if willWriteFiles:
+    outputFileName = '{}/{}.dot'.format(outputFolder, fileName)
+    outputFile = open(outputFileName, 'w')
+    outputFile.write('digraph {} {{'.format(fileName))
+    outputFile.write('nodesep=1')
+    outputFile.write('compound=true')
+    outputFile.write('}')
+    outputFile.close()
 
-renderFile = open('output/render_SampleSequenceInput.bat', 'w')
-renderFile.write('dot -Tsvg SampleSequenceInput.dot -o SampleSequenceInput.svg')
-renderFile.close()
+    if willWriteBatchFiles:
+        renderFileName = '{}/{}.bat'.format(outputFolder, fileName)
+        renderFile = open(renderFileName, 'w')
+        renderFile.write('dot -Tsvg {0}.dot -o {0}.svg', fileName)
+        renderFile.close()
 
